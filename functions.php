cab49f25a1d3823a8289089ab8aa80a78620707c
attelah <lahepela@arcada.fi>
2022-05-09 13:50:05 +0300

    första hooken

--- functions.php
+++ functions.php
@@ -11,16 +11,25 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
+define('CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0');
 
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
+function child_enqueue_styles()
+{
 
-	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+    wp_enqueue_style('astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
 
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
-?>
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
+
+function notificationsBar()
+{
+    echo '<div class="notificationsBar">';
+    echo 'Öppentiderna ändras fr.o.m. 1.6! Nya öppentiderna är: Mån-Lö 9 - 15';
+    echo '</div>';
+}
+
+add_filter('astra_above_header', 'notificationsBar');
